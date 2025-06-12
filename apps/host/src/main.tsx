537f350648c5b6911b5e97fb89fdecd678af2bd7
User <yonis676@gmail.com>
2025-06-13 02:11:07 +0300

    fixed deploy base config

--- apps/host/src/main.tsx
+++ apps/host/src/main.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter } from 'react-router-dom';
 import * as ReactDOM from 'react-dom/client';
 import App from './app/app';
+// @ts-ignore
+const base = import.meta.env.BASE_URL; // '/ai-invest-assistant/'
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={base}>
     <App />
   </BrowserRouter>
 );
